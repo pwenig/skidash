8c47a7897b7e3322537ac4b6eacb370898f9f7d6
Paul Wenig <pwenig@gmail.com>
2015-09-21 11:16:32 -0600

    Added more hourly API tests

--- spec/features/weather_api_spec.rb
+++ spec/features/weather_api_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 require 'httparty'
 
-feature "weather API" do
+feature "weather API_hourly" do
 
-  scenario "successfully connets to API" do
-      response = HTTParty.get("http://api.wunderground.com/api/#{ENV['WEATHER_API']}/hourly/q/80466.xml")
+  before response = Weather.new.fetch_weather_hourly(80466) do
+  end
 
+  scenario "successfully connets to API for hourly forecast" do
       expect(response.present?).to eq(true)
+  end
+
+  scenario "successfully retrieves hourly forecast" do
+     initial_response = response['response']['hourly_forecast']['forecast'].first
+     temperature = initial_response['temp']['english']
+     expect(temperature.present?).to eq(true)
+  end
+
+  scenario "successfully retrieves forecast icon" do
+    initial_response = response['response']['hourly_forecast']['forecast'].first
+    icon = initial_response['icon_url']
+    expect(icon.present?).to eq(true)
   end
 
 end
