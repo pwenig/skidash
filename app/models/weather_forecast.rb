1f1b2368f0285928d42a09d055d23e09b5138ccc
Paul Wenig <pwenig@gmail.com>
2015-09-21 20:20:30 -0600

    Added daily high and low temps

--- app/models/weather_forecast.rb
+++ app/models/weather_forecast.rb
@@ -1,7 +1,8 @@
 class WeatherForecast
   require 'httparty'
 
-  attr_accessor :forecast_today, :forecast_day2, :forecast_title_day2
+  attr_accessor :forecast_today, :forecast_day2, :forecast_title_day2, :forecast_title_day3, :forecast_day3,
+                :today_high, :today_low
 
   def initialize(zip)
     weather_forecast_hash = fetch_weather_forecast(zip)
@@ -14,9 +15,15 @@ class WeatherForecast
 
   def assign_forecast_values(weather_hash)
     forecast_response = weather_hash.parsed_response['response']['forecast']['txt_forecast']['forecastdays']['forecastday']
+    simple_forecast_response = weather_hash.parsed_response['response']['forecast']['simpleforecast']['forecastdays']['forecastday']
+
     self.forecast_today = forecast_response[0]['fcttext']
+    self.today_high = simple_forecast_response[0]['high']['fahrenheit']
+    self.today_low = simple_forecast_response[0]['low']['fahrenheit']
     self.forecast_title_day2 = forecast_response[2]['title']
     self.forecast_day2 = forecast_response[2]['fcttext']
+    self.forecast_title_day3 = forecast_response[4]['title']
+    self.forecast_day3 = forecast_response[4]['fcttext']
 
   end
 end
