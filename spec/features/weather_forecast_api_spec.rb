26894a21ed6f02243b2aba9d0285cf990c9098a3
Paul Wenig <pwenig@gmail.com>
2015-09-21 19:51:51 -0600

    Refactored test

--- spec/features/weather_forecast_api_spec.rb
+++ spec/features/weather_forecast_api_spec.rb
@@ -3,7 +3,7 @@ require 'httparty'
 
 feature "weather API forecast" do
 
-  before response = HTTParty.get("http://api.wunderground.com/api/#{ENV['WEATHER_API']}/forecast/q/80466.xml") do
+  before response = WeatherForecast.new(80466) do
 
   end
 
@@ -12,9 +12,11 @@ feature "weather API forecast" do
   end
 
   scenario "successfully retrieves forecast" do
-    forecast_response = response.parsed_response['response']['forecast']['txt_forecast']['forecastdays']['forecastday'].first
-    forecast_text = forecast_response['fcttext']
-    expect(forecast_text.present?).to eq(true)
+    expect(response.forecast_today.present?).to eq(true)
+  end
+
+  scenario "successfully retrieves forecast for tomorrow" do
+    expect(response.forecast_day2.present?).to eq(true)
   end
 
 end
