0121859d90ca1383626f54347c32f0d016128c15
Paul Wenig <pwenig@gmail.com>
2015-09-22 20:21:30 -0600

    Changed A-Basin to Vail

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,18 +3,18 @@ class DashboardController < ApplicationController
   ELD = 80466
   STE = 80487
   BRE = 80424
-  ABA = 80435
+  VAIL = 81657
 
   def index
   @weather_lookup_eld = WeatherHourly.new(ELD)
   @weather_lookup_ste = WeatherHourly.new(STE)
   @weather_lookup_bre = WeatherHourly.new(BRE)
-  @weather_lookup_aba = WeatherHourly.new(ABA)
+  @weather_lookup_vail = WeatherHourly.new(VAIL)
 
   @weather_forecast_lookup_eld = WeatherForecast.new(ELD)
   @weather_forecast_lookup_ste = WeatherForecast.new(STE)
   @weather_forecast_lookup_bre = WeatherForecast.new(BRE)
-  @weather_forecast_lookup_aba = WeatherForecast.new(ABA)
+  @weather_forecast_lookup_vail = WeatherForecast.new(VAIL)
 
   end
 end
