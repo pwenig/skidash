1e1ca7f4ed5173f71f776f2cbd6df0538c7f445a
Paul Wenig <pwenig@gmail.com>
2015-09-25 18:20:21 -0600

    Small refactor to controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,20 +1,35 @@
 class DashboardController < ApplicationController
 
-  ELD = 80466
-  STE = 80487
-  BRE = 80424
-  VAIL = 81657
-
   def index
-  @weather_lookup_eld = WeatherHourly.new(ELD)
-  @weather_lookup_ste = WeatherHourly.new(STE)
-  @weather_lookup_bre = WeatherHourly.new(BRE)
-  @weather_lookup_vail = WeatherHourly.new(VAIL)
-
-  @weather_forecast_lookup_eld = WeatherForecast.new(ELD)
-  @weather_forecast_lookup_ste = WeatherForecast.new(STE)
-  @weather_forecast_lookup_bre = WeatherForecast.new(BRE)
-  @weather_forecast_lookup_vail = WeatherForecast.new(VAIL)
+    weather_hourly_lookup
+    weather_forecast_lookup
 
   end
+
+
+  def weather_hourly_lookup
+
+    eld = Mountain.find_by_name("Eldora").zip_code
+    ste = Mountain.find_by_name("Steamboat").zip_code
+    bre = Mountain.find_by_name("Breckenridge").zip_code
+    vail = Mountain.find_by_name("Vail").zip_code
+
+    @weather_lookup_eld = WeatherHourly.new(eld)
+    @weather_lookup_ste = WeatherHourly.new(ste)
+    @weather_lookup_bre = WeatherHourly.new(bre)
+    @weather_lookup_vail = WeatherHourly.new(vail)
+  end
+
+  def weather_forecast_lookup
+    eld = Mountain.find_by_name("Eldora").zip_code
+    ste = Mountain.find_by_name("Steamboat").zip_code
+    bre = Mountain.find_by_name("Breckenridge").zip_code
+    vail = Mountain.find_by_name("Vail").zip_code
+
+    @weather_forecast_lookup_eld = WeatherForecast.new(eld)
+    @weather_forecast_lookup_ste = WeatherForecast.new(ste)
+    @weather_forecast_lookup_bre = WeatherForecast.new(bre)
+    @weather_forecast_lookup_vail = WeatherForecast.new(vail)
+  end
+
 end
