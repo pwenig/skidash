94a5a29f58e95a1bb5348dbea3512d251ce7b95b
Paul Wenig <pwenig@gmail.com>
2015-11-08 11:33:29 -0700

    Moved weather JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,12 +19,3 @@
 
 $('.jquery-background-video').bgVideo({fadeIn: 2000});
 
-function showWeather() {
-  var show_weather = document.getElementById('content');
-  var button = document.getElementById('button');
-  var radar = document.getElementById('radar');
-  show_weather.style.display = 'block';
-  button.style.display = 'none';
-  radar.style.display = 'block';
-
-}
