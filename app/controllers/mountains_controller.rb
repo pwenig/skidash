cc5910c1453f323a762cff4c63e98d8bae3813f0
Paul Wenig <pwenig@gmail.com>
2015-11-21 13:34:46 -0700

    Lookup mountain by id not zip

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -6,34 +6,32 @@ class MountainsController < ApplicationController
 
 
   def index
-    mountains_zip_codes = []
+    mountain_ids = []
     new_params = {}
     new_params[:mountains] = params[:mountain1], params[:mountain2], params[:mountain3], params[:mountain4]
-
     new_params[:mountains].each do |mountain|
       if mountain[:id].present?
-        mountain_zip = Mountain.find(mountain[:id]).zip_code
-        mountains_zip_codes << mountain_zip
+        mountain_ids << mountain[:id]
       end
     end
-    mountains_zip_codes
+    mountain_ids
 
-    weather_condition_lookup(mountains_zip_codes)
+    weather_condition_lookup(mountain_ids)
     travel_time
   end
 
 
-  def weather_condition_lookup(mountains_zip_codes)
+  def weather_condition_lookup(mountain_ids)
     mountain_info = {}
     @mountain_conditions = []
-    mountains_zip_codes.each do |zip|
-      mountain = Mountain.find_by_zip_code(zip)
+    mountain_ids.each do |id|
+      mountain = Mountain.find(id)
       mountain_info[:name] = mountain[:name]
       mountain_info[:video_url] = mountain[:video_url]
       mountain_info[:terrain_url] = mountain[:terrain_url]
       mountain_info[:runs_open] = mountain[:runs_open]
       mountain_info[:lifts_open] = mountain[:lifts_open]
-      weather_condition = WeatherCondition.new(zip)
+      weather_condition = WeatherCondition.new(mountain[:zip_code])
      @mountain_conditions << [mountain_info, weather_condition.condition_hash, weather_condition.forecast_hash].inject(:merge)
     end
 
